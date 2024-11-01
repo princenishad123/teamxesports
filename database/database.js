258cacb7c6ece4aea38ddf07bb1343119c378209
Prince Nishad <pn3147399@gmail.com>
2024-11-01 10:45:58 +0530

    Changes .env file

--- database/database.js
+++ database/database.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
-const DB_URL = "mongodb://127.0.0.1:27017/teamxesports"
+import dotenv from "dotenv";
+
+// Configure dotenv to load environment variables from .env file
+dotenv.config();
+
+const DB_URL = process.env.DATABASE_URL;
+
+console.log("Connecting to:", DB_URL);
+
 const databaseConnect = () => {
-    mongoose.connect(DB_URL, {
-         useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    }).then((res)=>console.log("database connected")).catch((err)=>console.log(err.message))
+ mongoose.connect(DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.error("Database connection error:", err));
 }
 
 export default databaseConnect;
